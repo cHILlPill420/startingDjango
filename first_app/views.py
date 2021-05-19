48de4ced972e126a6a33af6dd7d034c8c9a29245
cHILLPill420 <ankitraya.ar@outlook.com>
2021-05-19 11:04:06 +0545

    MTV paradigm basic

--- first_app/views.py
+++ first_app/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import AccessRecord, Topic, Webpage
 # Create your views here.
 
 def index(request):
-    my_dict = {'insert_me' : "hello I'm from views.py html=firstapp/index!"}
-    return render(request, 'first_app/index.html', context = my_dict)
+    # my_dict = {'insert_me' : "hello I'm from views.py html=firstapp/index!"}
+    # return render(request, 'first_app/index.html', context = my_dict)
+    webpage_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpage_list}
+    return render(request, 'first_app/index.html', context = date_dict)
+
 
 #def index2(request):
     #return HttpResponse("<em> 2nd index </em>")
