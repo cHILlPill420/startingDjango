7151ddcd64f2dd825e377c1e2f181456d43fd800
cHILLPill420 <ankitraya.ar@outlook.com>
2021-05-23 18:35:42 +0545

    used clean_ method for validation

--- first_app/forms.py
+++ first_app/forms.py
@@ -2,4 +2,11 @@ from django import forms
 class FormNorm(forms.Form):
     name = forms.CharField()
     text = forms.CharField(widget = forms.Textarea)
+    botcatcher = forms.CharField(required = False,
+                                 widget = forms.HiddenInput)
     
+    def clean_botcatcher(self):
+        botcatcher = self.cleaned_data['botcatcher']
+        if len(botcatcher)>0:
+            raise forms.ValidationError("Found bot")
+        return botcatcher
