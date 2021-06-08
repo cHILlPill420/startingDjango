fbe377d6e54a106519b7e78190f0bfbaca971c34
cHILLPill420 <ankitraya.ar@outlook.com>
2021-06-08 18:44:59 +0545

    template tags through Tempview: failed

--- first_app/views.py
+++ first_app/views.py
@@ -121,9 +121,9 @@ class CBView(View):
         return HttpResponse("Class Based Views")
 
 class TempCBView(TemplateView):
-    template_name = 'first_app/cbv.html'
+    template_name = 'cbv.html'
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context={'inject_me': 'Injection'}
+        context = super(TempCBView).get_context_data(**kwargs)
+        context['inject_me']='Basic Injection' 
         return context
